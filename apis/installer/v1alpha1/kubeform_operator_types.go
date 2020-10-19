2f15a6f6dbdd1de12579945ace8808f2f86edd4e
Tamal Saha <tamal@appscode.com>
2020-10-19 04:01:32 -0700

    Add license verification (#27)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/kubeform_operator_types.go
+++ apis/installer/v1alpha1/kubeform_operator_types.go
@@ -47,38 +47,40 @@ type KubeformOperatorSpec struct {
 	//+optional
 	NameOverride string `json:"nameOverride" protobuf:"bytes,1,opt,name=nameOverride"`
 	//+optional
-	FullnameOverride string       `json:"fullnameOverride" protobuf:"bytes,2,opt,name=fullnameOverride"`
-	ReplicaCount     int32        `json:"replicaCount" protobuf:"varint,3,opt,name=replicaCount"`
-	Operator         ContianerRef `json:"operator" protobuf:"bytes,4,opt,name=operator"`
-	ImagePullPolicy  string       `json:"imagePullPolicy" protobuf:"bytes,5,opt,name=imagePullPolicy"`
+	FullnameOverride string `json:"fullnameOverride" protobuf:"bytes,2,opt,name=fullnameOverride"`
+	ReplicaCount     int32  `json:"replicaCount" protobuf:"varint,3,opt,name=replicaCount"`
+	// +optional
+	License         string       `json:"license" protobuf:"bytes,4,opt,name=license"`
+	Operator        ContianerRef `json:"operator" protobuf:"bytes,5,opt,name=operator"`
+	ImagePullPolicy string       `json:"imagePullPolicy" protobuf:"bytes,6,opt,name=imagePullPolicy"`
 	//+optional
-	ImagePullSecrets []string `json:"imagePullSecrets" protobuf:"bytes,6,rep,name=imagePullSecrets"`
+	ImagePullSecrets []string `json:"imagePullSecrets" protobuf:"bytes,7,rep,name=imagePullSecrets"`
 	//+optional
-	CriticalAddon bool `json:"criticalAddon" protobuf:"varint,7,opt,name=criticalAddon"`
+	CriticalAddon bool `json:"criticalAddon" protobuf:"varint,8,opt,name=criticalAddon"`
 	//+optional
-	LogLevel int32 `json:"logLevel" protobuf:"varint,8,opt,name=logLevel"`
+	LogLevel int32 `json:"logLevel" protobuf:"varint,9,opt,name=logLevel"`
 	//+optional
-	Annotations map[string]string `json:"annotations" protobuf:"bytes,9,rep,name=annotations"`
+	Annotations map[string]string `json:"annotations" protobuf:"bytes,10,rep,name=annotations"`
 	//+optional
-	PodAnnotations map[string]string `json:"podAnnotations" protobuf:"bytes,10,rep,name=podAnnotations"`
+	PodAnnotations map[string]string `json:"podAnnotations" protobuf:"bytes,11,rep,name=podAnnotations"`
 	//+optional
-	NodeSelector map[string]string `json:"nodeSelector" protobuf:"bytes,11,rep,name=nodeSelector"`
+	NodeSelector map[string]string `json:"nodeSelector" protobuf:"bytes,12,rep,name=nodeSelector"`
 	// If specified, the pod's tolerations.
 	// +optional
-	Tolerations []core.Toleration `json:"tolerations" protobuf:"bytes,12,rep,name=tolerations"`
+	Tolerations []core.Toleration `json:"tolerations" protobuf:"bytes,13,rep,name=tolerations"`
 	// If specified, the pod's scheduling constraints
 	// +optional
-	Affinity *core.Affinity `json:"affinity" protobuf:"bytes,13,opt,name=affinity"`
+	Affinity *core.Affinity `json:"affinity" protobuf:"bytes,14,opt,name=affinity"`
 	// PodSecurityContext holds pod-level security attributes and common container settings.
 	// Optional: Defaults to empty.  See type description for default values of each field.
 	// +optional
-	PodSecurityContext *core.PodSecurityContext `json:"podSecurityContext" protobuf:"bytes,14,opt,name=podSecurityContext"`
-	ServiceAccount     ServiceAccountSpec       `json:"serviceAccount" protobuf:"bytes,15,opt,name=serviceAccount"`
+	PodSecurityContext *core.PodSecurityContext `json:"podSecurityContext" protobuf:"bytes,15,opt,name=podSecurityContext"`
+	ServiceAccount     ServiceAccountSpec       `json:"serviceAccount" protobuf:"bytes,16,opt,name=serviceAccount"`
 	//+optional
-	SecretKey *string `json:"secretKey" protobuf:"bytes,16,opt,name=secretKey"`
+	SecretKey *string `json:"secretKey" protobuf:"bytes,17,opt,name=secretKey"`
 	//+optional
-	EnableAnalytics bool      `json:"enableAnalytics" protobuf:"varint,17,opt,name=enableAnalytics"`
-	Proxy           ProxySpec `json:"proxy" protobuf:"bytes,18,opt,name=proxy"`
+	EnableAnalytics bool      `json:"enableAnalytics" protobuf:"varint,18,opt,name=enableAnalytics"`
+	Proxy           ProxySpec `json:"proxy" protobuf:"bytes,19,opt,name=proxy"`
 }
 
 type ImageRef struct {
